108d164a854e0e559874da09957e2ab3422faa35
Shifeng Xu <xsfpku@gmail.com>
2020-05-31 14:09:09 +0800

    Init IdentityServer project.

---
+++ 1_ClientCredentials/IdentityServer/src/IdentityServer/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer4";
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
