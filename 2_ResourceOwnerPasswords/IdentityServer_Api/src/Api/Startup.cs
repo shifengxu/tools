6ada546c58f10d3ce82d044e2ee6a3e12acf2ba1
Shifeng Xu <xsfpku@gmail.com>
2020-06-02 23:59:36 +0800

    Init 2_ResourceOwnerPasswords

---
+++ 2_ResourceOwnerPasswords/IdentityServer_Api/src/Api/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "api1";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
