1198dc5d90f60722401910af111271bb984cb496
Shifeng Xu <xsfpku@gmail.com>
2020-06-04 20:09:46 +0800

    Init 4_ImplicitFlowAuthenticationWithExternal

---
+++ 4_ImplicitFlowAuthenticationWithExternal/IdentityServer/src/IdentityServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer4";
+
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
+        }
+    }
+}
