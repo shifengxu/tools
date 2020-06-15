53eb55a587bb277ab76ac1a8f35f0caf7cb20a72
ASCENTIS\shifeng <shifeng.xu@ascentis.com.sg>
2020-06-15 18:11:31 +0800

    Init: Selenium example on Windows with Specflow

---
+++ Windows/SpecflowSeleniumXUnitTestProj/TestHooks.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace SpecflowSeleniumXUnitTestProj
+{
+    [Binding]
+    public class TestHooks
+    {
+        [Before]
+        //[BeforeTestRun]
+        public void CreateWebDriver(ScenarioContext context)
+        {
+            // We are using Chrome, but you can use any driver
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-notifications");
+
+            IWebDriver webDriver = new ChromeDriver(options);
+            context["WEB_DRIVER"] = webDriver;
+        }
+
+        [After]
+        //[AfterTestRun]
+        public void CloseWebDriver(ScenarioContext context)
+        {
+            var driver = context["WEB_DRIVER"] as IWebDriver;
+            driver.Quit();
+        }
+    }
+}
